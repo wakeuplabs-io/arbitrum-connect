60b94231ec6929006a861faba48c3a3a128b3296
Nahuel Jaume <nahueljaume@gmail.com>
2025-01-08 15:59:54 -0300

    include shared chains on getUserCHains

--- packages/ui/src/contexts/custom-chain.tsx
+++ packages/ui/src/contexts/custom-chain.tsx
@@ -79,7 +79,7 @@ export function CustomChainProvider({ children }: { children: ReactNode }) {
       ? JSON.parse(storedChains)
       : [];
     setChains(
-      parsedChains.filter((chain: CustomChain) => chain.user === userAddress),
+      parsedChains.filter((chain: CustomChain) => chain.user === userAddress || chain.user === zeroAddress),
     );
     setLoading(false);
   };
