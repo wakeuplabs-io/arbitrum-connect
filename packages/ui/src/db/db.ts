10d4f07e1aeb701f25170b47d7436aa8ebb8f094
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2025-01-31 12:47:51 -0300

    removed unnecesary db initialization

--- packages/ui/src/db/db.ts
+++ packages/ui/src/db/db.ts
@@ -1,5 +1,4 @@
 import { Transaction } from '@/lib/transactions';
-import { customMainnet, customSepolia, customArbitrum, customArbitrumSepolia } from '@/lib/wagmi-config';
 import { CustomChain } from '@/types';
 import Dexie, { type EntityTable } from 'dexie';
 
@@ -17,18 +16,5 @@ db.version(1).stores({
     transactions: 'bridgeHash, [account+bridgeHash]'
 });
 
-async function initializeChains() {
-    const count = await db.chains.count();
-    if (count === 0) {
-        await db.chains.bulkAdd([
-            { ...customMainnet, id: undefined },
-            { ...customSepolia, id: undefined },
-            { ...customArbitrum, id: undefined },
-            { ...customArbitrumSepolia, id: undefined }
-        ]);
-    }
-}
-
-initializeChains().catch(console.error);
 
 export { db };
