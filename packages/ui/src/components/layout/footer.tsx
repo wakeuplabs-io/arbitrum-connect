6d3c7e93aa8bc51f62af06773c96519805c2cfee
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2025-04-25 14:11:55 -0300

    improve responsiveness on activity view

--- packages/ui/src/components/layout/footer.tsx
+++ packages/ui/src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import { WakeupSignature } from "@/components/wakeup-signature";
 
 export function Footer() {
   return (
-    <footer className="flex justify-center items-center | sm:px-8 | h-[4rem] z-10 bg-white px-4">
+    <footer className="flex justify-center items-center | sm:px-8 | h-[3rem] sm:h-[4rem] z-10 bg-white px-4">
       <WakeupSignature />
     </footer>
   );
